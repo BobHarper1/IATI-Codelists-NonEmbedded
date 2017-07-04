1f956a14832612355f1d8080b5e9eeb118772e15
Andy Lulham <a.lulham@gmail.com>
2017-07-04 17:31:05 +0100

    Fetch a bunch of relevant codelist data

--- get.sh
+++ get.sh
@@ -1,1 +1,18 @@
 wget "https://www.iana.org/assignments/media-types/media-types.xml" -O source/media-types.xml
+
+wget "https://raw.githubusercontent.com/datasets/currency-codes/master/data/codes-all.csv" -O source/currencies.csv
+
+wget "https://raw.githubusercontent.com/datasets/country-codes/master/data/country-codes.csv" -O source/countries.csv
+
+wget "https://raw.githubusercontent.com/datasets/language-codes/master/data/language-codes-full.csv" -O source/languages.csv
+
+wget "https://raw.githubusercontent.com/andylolz-org/dac-crs-codes/master/data/aid_types.csv" -O source/dac_crs_aid_types.csv
+wget "https://raw.githubusercontent.com/andylolz-org/dac-crs-codes/master/data/aid_type_categories.csv" -O source/dac_crs_aid_type_categories.csv
+wget "https://raw.githubusercontent.com/andylolz-org/dac-crs-codes/master/data/collaboration_types.csv" -O source/dac_crs_collaboration_types.csv
+wget "https://raw.githubusercontent.com/andylolz-org/dac-crs-codes/master/data/channel_codes.csv" -O source/dac_crs_channel_codes.csv
+wget "https://raw.githubusercontent.com/andylolz-org/dac-crs-codes/master/data/finance_types.csv" -O source/dac_crs_finance_types.csv
+wget "https://raw.githubusercontent.com/andylolz-org/dac-crs-codes/master/data/finance_type_categories.csv" -O source/dac_crs_finance_type_categories.csv
+wget "https://raw.githubusercontent.com/andylolz-org/dac-crs-codes/master/data/flow_types.csv" -O source/dac_crs_flow_types.csv
+wget "https://raw.githubusercontent.com/andylolz-org/dac-crs-codes/master/data/recipients.csv" -O source/dac_crs_regions.csv
+wget "https://raw.githubusercontent.com/andylolz-org/dac-crs-codes/master/data/sectors.csv" -O source/dac_crs_sectors.csv
+wget "https://raw.githubusercontent.com/andylolz-org/dac-crs-codes/master/data/sector_categories.csv" -O source/dac_crs_sector_categories.csv
